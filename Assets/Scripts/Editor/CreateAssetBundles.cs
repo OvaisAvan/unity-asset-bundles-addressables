0cb8b32b511c27ce8b3cb7a2df3912b7882a9355
Owais <ovaisavan1996@gmail.com>
2026-05-02 03:13:20 +0500

    Basic function of editor

---
+++ Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundles 
+{
+    [MenuItem("BlackSpider Studios/Create/AssetBundles")]
+    private static void BuildAssetBundles()
+    {
+        Debug.Log("Build AssetBundles");
+    }
+}
