f8daed69edf5ae0cae7d26c33cd6b1bab70d30a0
Owais <ovaisavan1996@gmail.com>
2026-05-02 04:43:46 +0500

    more delegates

--- Assets/Scripts/Callback.cs
+++ Assets/Scripts/Callback.cs
@@ -1,2 +1,4 @@
 public delegate void CallBack();
-public delegate void CallBack<T>(T arg);
+public delegate void CallBack<T>(T arg1);
+public delegate void CallBack<T1, T2>(T1 arg1, T2 arg2);
+public delegate void CallBack<T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3);
